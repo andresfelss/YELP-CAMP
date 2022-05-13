bb3b45b9534e7edfc850caf8e9b6ceb90c23a26e
andre <andres.felipepr17@gmial.com>
2022-05-13 18:35:57 -0500

    Set images description and price for our seeds of the DB

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const seedDB = async()=>{
     await Campground.deleteMany({});
     for(let i = 0; i<50;i++){
         const random100 = Math.floor(Math.random()* 1000);
+        const price = Math.floor(Math.random()*20)+100;
         const camp = new Campground({
             location: `${cities[random100].city}, ${cities[random100].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui neque necessitatibus harum voluptatibus, veniam nemo veritatis at soluta iusto perspiciatis dolorum dignissimos placeat, nulla aperiam esse suscipit blanditiis aut consectetur            Libero quae, omnis quibusdam doloremque dignissimos, quas magni reprehenderit, tenetur repellendus labore odit earum asperiores laudantium sit. Officia cumque beatae ut, non, dolorem nisi voluptatibus culpa magnam, placeat odio repudiandae?",
+            price: price
         });
         await camp.save();
     }
