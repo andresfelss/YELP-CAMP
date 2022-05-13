0e466b6ee8a3195d4a1ee72305ee35c5cf557454
andre <andres.felipepr17@gmial.com>
2022-05-13 10:58:50 -0500

    Commenting my code

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+// Modulos que necesitamos importar
 const { Console } = require('console');
 const express = require('express');
 const mongoose  = require('mongoose');
 const path =require('path');
 const methodOverride = require('method-override');
+const main = require('./config/mongo'); // llamo la configuracion de mi Base de Datos
+// ---------------- El codigo empieza aca ---------------------------------------------
+const app = express(); // Inicio mi express app
+app.use(express.urlencoded({extended: true})); // Para que reciba elementos en el body
 
-
-const app = express();
-const main = require('./config/mongo');
-
-app.use(express.urlencoded({extended: true}));
-
+// Configuracion EJS View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views')); // Directorio
 
 // Config Method- override
 app.use(methodOverride('_method'));
@@ -17,19 +19,17 @@ app.use(methodOverride('_method'));
 // DATA BASE CONFIG
 main().catch(e => console.log(e));
 
+app.get('/', (req,res)=>{
+    res.render('home'); // Mi HTML de Home
+})
 
-// Configuracion EJS View Engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views')); // Directorio
 
 // Para cargar de manera dinamica las rutas
 app.use("/api", require('./routes'));
 
-app.get('/', (req,res)=>{
-    res.render('home');
-})
 
 
+// EL listen siempre al final
 app.listen(3000, ()=>{
     console.log('Serving On port 3000: http://localhost:3000/ ')
 })
