fecba7cd145ad13e36be41c7bf362d2363cfbf8d
andre <andres.felipepr17@gmial.com>
2022-05-13 08:26:04 -0500

    Initial Connection with MongoDB succes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const { Console } = require('console');
 const express = require('express');
+const mongoose  = require('mongoose');
 const path =require('path');
 
 const app = express();
+
+
+// DATA BASE CONFIG
+main().catch(e => console.log(e));
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-campDB');
+    console.log('Connection Succes');
+}
 
 // Configuracion EJS View Engine
 app.set('view engine', 'ejs');
