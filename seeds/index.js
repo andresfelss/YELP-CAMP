45db28d7ca8a5fbfef2331240e601359af03c7ad
andre <andres.felipepr17@gmial.com>
2022-05-13 09:17:04 -0500

    Finish basic seed setup

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,30 @@
+const { default: mongoose } = require('mongoose');
 const main = require('../config/mongo');
 const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } =require('./seedHelpers')
 
 // DATA BASE CONFIG
 main().catch(e => console.log(e));
 
+
+const sample = (array) => array[Math.floor(Math.random()*array.length)];
+
+
 // Elimino TODO de mi base de datos
 const seedDB = async()=>{
     await Campground.deleteMany({});
-    const c = new Campground({title: 'Test'});
-    await c.save();
+    for(let i = 0; i<50;i++){
+        const random100 = Math.floor(Math.random()* 1000);
+        const camp = new Campground({
+            location: `${cities[random100].city}, ${cities[random100].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        });
+        await camp.save();
+    }
 }
 
 
-seedDB();
+seedDB().then(() =>{
+    mongoose.connection.close();
+});
