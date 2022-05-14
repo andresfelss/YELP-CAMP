ae3923d4838cc2c9554e75c524d23e7bbc48908f
andre <andres.felipepr17@gmial.com>
2022-05-14 12:06:00 -0500

    Making my Joi validator Schema a middleware also created the validator folder

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,37 +4,19 @@ const { findByIdAndUpdate, findByIdAndDelete } = require('../models/campground')
 const Campground = require('../models/campground');
 const router = express.Router();
 const cathcAsync = require('../helpers/catchAsync');
-
-const Joi = require('joi');
 const ExpressError = require('../helpers/ExpressErrors');
-//joi
+const validateCampground = require('../validators/campgroundValidator')
+
 
 // Crear Campground
 router.get('/new', (req,res)=>{
   res.render('campgrounds/new');
 })
-router.post('/',cathcAsync(async(req,res)=>{
-  const campgroundSchema = Joi.object({
-    campground: Joi.object({
-      title: Joi.string().required(),
-      price: Joi.number().required().min(0),
-    }).required()
-  })
-
-  const  {error} = campgroundSchema.validate(req.body);
-  if(error){
-    const msg =error.details.map(el => el.message).join(',')
-    throw new ExpressError(msg,400)
-  }
-
+router.post('/',validateCampground,cathcAsync(async(req,res)=>{
   const camp = new Campground(req.body.campground);
   await camp.save();
   res.redirect(`campgrounds/${camp._id}`);
 }))
-
-
-
-
 
 /**
  * Listar Campgrounds
@@ -49,7 +31,6 @@ router.get('/:id', cathcAsync(async(req,res)=>{
   res.render('campgrounds/show' , {camp});
 }));
 
-
 /**
  * Edit Campgrpund
  */
@@ -58,7 +39,7 @@ router.get('/:id/edit', cathcAsync(async(req,res) => {
   const camp = await Campground.findById(id);
   res.render('campgrounds/edit' , {camp});
 }));
-router.put('/:id', cathcAsync(async(req,res) =>{
+router.put('/:id',validateCampground, cathcAsync(async(req,res) =>{
   const {id} = req.params;
   const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {runValidators:true});
   res.redirect(`/api/campgrounds/${camp._id}`);
