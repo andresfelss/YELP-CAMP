7a40170272b4bad6a7e625a6464a6f657a339a09
andre <andres.felipepr17@gmial.com>
2022-05-15 09:24:16 -0500

    Server side JOI validation to review

--- validators/reviewsValidator.js
+++ validators/reviewsValidator.js
@@ -5,7 +5,7 @@ const validateReview = (req,res,next)=>{
     const reviewSchema = Joi.object({
         review: Joi.object({
             body: Joi.string().required(),
-            rating: Joi.number().required()
+            rating: Joi.number().required().min(1).max(5)
         }).required()
     })
     const  {error} = reviewSchema.validate(req.body);
