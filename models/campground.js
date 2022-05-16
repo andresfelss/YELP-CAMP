fd99a2ba57cc9ab5a9914c3b3124a3876787b9e4
andre <andres.felipepr17@gmial.com>
2022-05-16 15:05:08 -0500

    thumbnail virtual

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxglelovzd2f3dgcpass.png
+
+const ImgSchema = new Schema({
+    url: String,
+    filename: String
+});
+ImgSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_200');  // no lo guardamos es solo una URL mirar (virtual property)
+});
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImgSchema],
     price: Number,
     description: String,
     location: String,
