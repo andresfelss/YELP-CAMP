25637a4c87d008ec8a9849a0b6488c76c42e994a
andre <andres.felipepr17@gmial.com>
2022-05-16 16:59:16 -0500

    Centering the map on A campground

--- public/showPageMap.js
+++ public/showPageMap.js
@@ -1,7 +1,11 @@
+
 mapboxgl.accessToken = mapToken;
   const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4 // starting zoom
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 11 // starting zoom
 });
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
