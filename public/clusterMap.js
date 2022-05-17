9ad89674b06678dbd0c3f981f698f03325c06bcb
andre <andres.felipepr17@gmial.com>
2022-05-17 11:46:22 -0500

    Change the center of cluster map

--- public/clusterMap.js
+++ public/clusterMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "cluster-map",
   style: "mapbox://styles/mapbox/dark-v10",
-  center: [-103.5917, 40.6699],
+  center: [-74.297, 4.570868],
   zoom: 3
 });
 map.addControl(new mapboxgl.NavigationControl());
